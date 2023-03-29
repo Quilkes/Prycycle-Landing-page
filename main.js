e39b98a7c24a3fb7c4189dbdd585134d5813f558
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-03-29 21:46:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,33 @@ const initApp = () => {
 
     menuBtn.addEventListener('click', toggleBtn)
     navOpen.addEventListener('click', toggleBtn)
+
+    const headline = document.getElementById('headline');
+const cursor = document.getElementById('cursor');
+const text = headline.innerText;
+
+function eraseText() {
+  headline.innerText = headline.innerText.slice(0, -1);
+  if (headline.innerText.length === 0) {
+    clearInterval(intervalId);
+    setTimeout(writeText, 1000);
+  }
+}
+
+function writeText() {
+  headline.innerText = '';
+  let i = 0;
+  intervalId = setInterval(() => {
+    headline.innerText += text.charAt(i);
+    i++;
+    if (i === text.length) {
+      clearInterval(intervalId);
+    }
+  }, 100);
+}
+
+let intervalId = setInterval(eraseText, 100);
+
 }
 
 const toggleBtn = () =>{
