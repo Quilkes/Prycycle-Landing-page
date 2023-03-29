b1ec57dbb287ac4666dd6e9ecf8c8622c57a9d17
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-03-29 23:19:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,53 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    initApp();
-})
-
 const initApp = () => {
+  const menuBtn = document.querySelector('.menu');
+  const navOpen = document.querySelector('nav');
+
+  menuBtn.addEventListener('click', toggleBtn);
+  navOpen.addEventListener('click', toggleBtn);
+
+  const headline = document.getElementById('headline');
+  const cursor = document.getElementById('cursor');
+  let text = headline.innerText;
+
+  function erase() {
+    let interval = setInterval(function() {
+      if (text.length == 0) {
+        clearInterval(interval);
+      } else {
+        text = text.slice(0, -1);
+        headline.innerText = text;
+      }
+    }, 50);
+  }
+
+  function write() {
+    let index = 0;
+    let interval = setInterval(function() {
+      if (index == text.length) {
+        clearInterval(interval);
+      } else {
+        let char = text.charAt(index);
+        if (char == " ") {
+          headline.innerHTML += "&nbsp;";
+        } else {
+          headline.innerHTML += char;
+        }
+        index++;
+      }
+    }, 50);
+  }
+
+  headline.addEventListener('click', () => {
+    erase();
+    setTimeout(() => {
+      write();
+    }, 500);
+  });
+
+  const toggleBtn = () => {
     const menuBtn = document.querySelector('.menu');
-    const navOpen = document.querySelector ('nav');
-
-    menuBtn.addEventListener('click', toggleBtn);
-    navOpen.addEventListener('click', toggleBtn);
-
-    const headline = document.getElementById('headline');
-    const cursor = document.getElementById('cursor');
-    const text = headline.innerText;
-
-    function erase() {
-        let text = document.querySelector('#text').innerHTML;
-        let interval = setInterval(function() {
-            if (text.length == 0) {
-                clearInterval(interval);
-            } else {
-                text = text.slice(0, -1);
-                document.querySelector('#text').innerHTML = text;
-            }
-        }, 50);
-    }
-
-
-    function write() {
-        let text = 'Bicycles, Skateboards, and Accessories!!!';
-        let index = 0;
-        let interval = setInterval(function() {
-            if (index == text.length) {
-                clearInterval(interval);
-            } else {
-                let char =text.charAt(index);
-                if (char == "") {
-                    document.querySelector('#text').innerHTML += "&nbsp;";
-                } else {
-                    document.querySelector('#text').innerHTML += char;
-                }
-                index++;
-            }
-        }, 50) 
-    }
-
-    const toggleBtn = () =>{
-        const menuBtn = document.querySelector('.menu');
-        const navOpen = document.querySelector ('nav');
-        menuBtn.classList.toggle('open')
-        navOpen.classList.toggle('open')
-    }
+    const navOpen = document.querySelector('nav');
+    menuBtn.classList.toggle('open');
+    navOpen.classList.toggle('open');
+  };
 }
