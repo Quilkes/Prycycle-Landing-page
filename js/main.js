811f5abde4d28d83a648ff1ce3aa9c753842eeeb
Banniroy <matinadekola33@gmail.com>
2023-06-16 18:57:01 +0100

    new update on index.html

--- js/main.js
+++ js/main.js
@@ -15,44 +15,4 @@ const initApp = () => {
 
     menuBtn.addEventListener('click', toggleBtn);
     navOpen.addEventListener('click', toggleBtn);
-
-    // const headline = document.getElementById('headline');
-    // const cursor = document.getElementById('cursor');
-    // const text = headline.innerText;
-
-    // function erase() {
-    //     let interval = setInterval(function() {
-    //         if (text.length == 0) {
-    //             clearInterval(interval);
-    //         } else {
-    //             text = text.slice(0, -1);
-    //             headline.innerHTML = text;
-    //         }
-    //     }, 50);
-    // }
-
-    // function write() {
-    //     let index = 0;
-    //     let interval = setInterval(function() {
-    //         if (index == text.length) {
-    //             clearInterval(interval);
-    //         } else {
-    //             let char =text.charAt(index);
-    //             if (char == '') {
-    //                 headline.innerHTML += "&nbsp;";
-    //             } else{
-    //                 headline.innerHTML += char;
-    //             }
-    //             index++;
-    //         }
-    //     }, 50) 
-    // }
-
-    // headline.addEventListener('click', () =>{
-    //     erase();
-    //     setTimeout(() =>{
-    //         write();
-    //     }, 500);
-    // });
-
 }
