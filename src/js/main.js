1c4b30031a2b8999ff134c82932130bf268ee7de
Banniroy <matinadekola33@gmail.com>
2023-08-17 19:52:57 +0100

    latset upgrade tailwind

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    initApp();
+});
+
+const initApp = () => {
+    const menuBtn = document.getElementById('mobile-open-btn');
+    const navOpen = document.getElementById('mobile-nav');
+
+    const toggleBtn = () => {
+        navOpen.classList.toggle('hidden');
+        navOpen.classList.toggle('flex');
+    };
+
+    menuBtn.addEventListener('click', toggleBtn);
+    navOpen.addEventListener('click', toggleBtn);
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Raleway: ['Raleway', 'sans-sherif'],
+        Phudu: ['Phudu', 'cursive'],
+        Roboto: ['Roboto', 'sans-sherif'],
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' }
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.3s ease-in-out forwards',
+      }
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
