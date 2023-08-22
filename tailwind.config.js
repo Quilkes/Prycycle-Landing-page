57b757b5ca6d433ea2a1de5047f70b8bb6f84f81
swiper(...) <matinadekola33@gmail.com>
2023-08-22 04:17:12 -0700

    new updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,6 @@ module.exports = {
           '0%': { transform: 'translateX(-100%)' },
           '100%': { transform: 'translateX(0)' }
         },
-      },
-      animation: {
-        'open-menu': 'open-menu 0.3s ease-in-out forwards',
       }
     },
   },
